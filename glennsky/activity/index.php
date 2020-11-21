a0d854f5542a44aba4d4b47f9733996a7b841be9
bonjo1 <70815595+bonjo1@users.noreply.github.com>
2020-11-21 20:52:56 +0800

    Add files via upload

---
+++ glennsky/activity/index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>Glenn's Site</title>
+    <link href="style.css" rel="stylesheet" type="text/css"
+  </head>
+  <body>
+      <header>
+
+        <div class="row">
+        <ul class="main-nav">
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="">Mod1</a></li>
+            <li><a href="">Mod2</a></li>
+            <li><a href="">Mod3</a></li>
+            <li><a href="">Mod4</a></li>
+            <li><a href="">Mod5</a></li>
+            <li><a href="mod6/index.php">Mod6</a></li>
+        </ul>
+        </div>
+
+
+
+
+
+      </header>
+
+
+
+
+  </body>
+</html>
---
+++ glennsky/activity/mod6/index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <title>Glenn's Site</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+      <header>
+
+        <div class="row">
+        <ul class="main-nav">
+            <li><a href="../../index.php">Home</a></li>
+            <li><a href="6-1/index.php">6-1</a></li>
+            <li><a href="6-2/index.php">6-2</a></li>
+
+        </ul>
+        </div>
+
+
+
+
+
+      </header>
+
+
+
+
+  </body>
+</html>
