67cc8fb47f2047b3e284cff95e08836b4f72795a
bonjo1 <70815595+bonjo1@users.noreply.github.com>
2020-11-21 21:10:50 +0800

    Add files via upload

---
+++ glennsky/index.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Glenn's Site</title>
+    <link href="style.css" rel="stylesheet" type="text/css"
+  </head>
+  <body>
+      <header>
+
+        <div class="row">
+        <ul class="main-nav">
+            <li><a href="">Home</a></li>
+            <li><a href="Exam/CLE1 (Midterm)/index.php">Exam</a></li>
+            <li><a href="activity/index.php">Activity</a></li>
+        </ul>
+        </div>
+
+
+
+
+
+      </header>
+
+
+
+
+  </body>
+</html>
